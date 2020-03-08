443aeb47f9225b63ae6e0c5c8047810c2f792bdb
Halis Duraki <duraki@linuxmail.org>
2020-03-08 16:34:01 +0100

    Initial Commit

---
+++ neoscreen/neoscreenView.h
@@ -0,0 +1,13 @@
+//
+//  neoscreenView.h
+//  neoscreen
+//
+//  Created by Halis Duraki on 08/03/2020.
+//  Copyright © 2020 Halis Duraki. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface neoscreenView : ScreenSaverView
+
+@end
---
+++ neoscreen/neoscreenView.m
@@ -0,0 +1,52 @@
+//
+//  neoscreenView.m
+//  neoscreen
+//
+//  Created by Halis Duraki on 08/03/2020.
+//  Copyright © 2020 Halis Duraki. All rights reserved.
+//
+
+#import "neoscreenView.h"
+
+@implementation neoscreenView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
